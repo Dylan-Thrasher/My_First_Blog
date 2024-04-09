4351fb463bfdf232952d69aa43ba0b2e09fd4f6d
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-04-09 15:00:36 -0400

    added pseudocode for logic js

---
+++ assets/js/logic.js
@@ -0,0 +1,8 @@
+
+// function to handle toggling dark mode
+
+//      if checking if current mode is dark or light
+
+//      swap mode
+
+// function to apply the css changes based off current mode
