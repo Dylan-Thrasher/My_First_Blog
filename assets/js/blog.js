c77cca39f96869d8e076f2cca0899abff4fceac5
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-04-09 16:39:03 -0400

    updated details to pseudocode in blog

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -4,3 +4,22 @@
 // check if blogposts are null
 
 // loop through blogposts and display them on page
+
+// create a div element
+// create a h1 element
+// create a p element
+// create a h2 element
+
+// text content of h1 = newContent[i].title
+// text content of p = newContent[i].content
+// text content of h2 = newContent[i].username
+
+// add class to h1
+// add class to p
+// add class to h2
+
+// append h1 to div
+// append p to div
+// append h2 to div
+
+// append div to page
