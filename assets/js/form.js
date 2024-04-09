63e5a4cf78e735a24dbb7cfe3f42d2d19b10ca7b
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-04-09 14:54:52 -0400

    added pseudocode for order of code in form js

---
+++ assets/js/form.js
@@ -0,0 +1,9 @@
+// on submit get data from users inputs and store into an object
+
+// check if users inputs are null using ! operator
+
+// get data from local storage of old blog posts
+
+// push new blog post into array of old blog posts
+
+// store new array of blog posts into local storage
