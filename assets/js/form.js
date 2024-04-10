39129a53a8144ceda639437df554bf97100c8559
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-04-09 21:06:08 -0400

    added information storing to local storage in form js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,24 @@
 // on submit get data from users inputs and store into an object
+let usernameInput = document.querySelector('#username');
+let titleInput = document.querySelector('#title');
+let contentInput = document.querySelector('#contect');
+const submitBtn = document.querySelector('#submit');
+
+submitBtn.addEventListener('click', function storeInfo() {
+    const stringInfo = localStorage.getItem('allPosts');
+    const allPosts = JSON.parse(stringInfo) || [];
+    
+    const enteredPost = {
+        Username: usernameInput.value,
+        Title: titleInput.value, 
+        Content: contentInput.value
+    };
+    allPosts.push(enteredPost);
+    localStorage.setItem('allPosts', JSON.stringify(allPosts));
+});
 
 // check if users inputs are null using ! operator
+
 
 // get data from local storage of old blog posts
 
