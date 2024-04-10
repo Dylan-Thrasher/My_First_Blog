488513faaeb4bda169243e2d91cef2322ac62a5b
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-04-09 20:47:57 -0400

    updated logic for light and dark mode

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,14 @@
-
+const modeSwitch = document.querySelector('#switch')
+let mode = 'light'
 // function to handle toggling dark mode
-
-//      if checking if current mode is dark or light
-
-//      swap mode
-
-// function to apply the css changes based off current mode
+modeSwitch.addEventListener('click', function() {
+    //      if checking if current mode is dark or light
+if (mode === 'light') {
+document.querySelector(':root').style.setProperty('color-scheme','dark');
+mode = 'dark'  
+//      swap mode  
+} else {
+    document.querySelector(':root').style.setProperty('color-scheme','light');
+    mode = 'light'    
+}
+});
