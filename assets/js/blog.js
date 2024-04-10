70fcd4a144f63a375723aedcbcc048d334b8a3bf
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-04-09 22:22:10 -0400

    updated blog javaScript for posting locally stored information

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,25 +1,45 @@
 
 // get blog posts from localstorage
-
-// check if blogposts are null
-
+const displayPosts = function () {
+    const stringInfo = localStorage.getItem('allPosts');
+    const newContent = JSON.parse(stringInfo);
+    const post = document.getElementById('main');
 // loop through blogposts and display them on page
+for (let i = 0; i < newContent.length; i++) {
+    // create a div element
+    // create a h1 element
+    // create a p element
+    // create a h2 element
+    let div = document.createElement('div');
+    let title = document.createElement('h1');
+    let content = document.createElement('p');
+    let author = document.createElement('h2');
 
-// create a div element
-// create a h1 element
-// create a p element
-// create a h2 element
+    // text content of h1 = newContent[i].title
+    // text content of p = newContent[i].content
+    // text content of h2 = newContent[i].username
+    title.textContent = newContent[i].Title;
+    content.textContent = newContent[i].Content;
+    author.textContent = 'Shared by: ' + newContent[i].Username;
 
-// text content of h1 = newContent[i].title
-// text content of p = newContent[i].content
-// text content of h2 = newContent[i].username
-
-// add class to h1
-// add class to p
-// add class to h2
+    // add class to h1
+    // add class to p
+    // add class to h2
+    div.setAttribute('class', 'post');
+    title.setAttribute('class', 'title');
+    content.setAttribute('class', 'content');
+    author.setAttribute('class', 'username');
 
 // append h1 to div
 // append p to div
 // append h2 to div
+    div.append(title);
+    div.append(content);
+    div.append(author);
 
 // append div to page
+    post.append(div);
+}
+}
+// check if blogposts are null
+displayPosts()
